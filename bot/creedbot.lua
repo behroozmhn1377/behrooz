de2a00a95aa10a5c083c4dde23c9304c29915f84
behroozmhn1377 <www.behrooz.mhn1377@gmail.com>
2016-06-04 21:40:01 +0430

    Update creedbot.lua

--- bot/creedbot.lua
+++ bot/creedbot.lua
@@ -239,7 +239,7 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {89313657,113741799},--Sudo users
+    sudo_users = {131853506},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
